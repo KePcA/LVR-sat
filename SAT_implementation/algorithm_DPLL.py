b21144da9e007ac652632983944d6af8aabbaca6
KePcA <igorklepic@gmail.com>
2014-03-28 12:14:48 +0100

    -added SAT_brute_force -finished DPLL algorithm (basic)

--- SAT_implementation/algorithm_DPLL.py
+++ SAT_implementation/algorithm_DPLL.py
@@ -12,46 +12,63 @@ import algorithm_utilities as au
 #Implementation of DPLL - argument is formula in CNF form. Returns False if formula is not in SAT or
 # values of variables otherwise
 
-def DPLL(CNF_formula):
+def DPLL(formula):
 
+    CNF_formula = au.cnf_nnf(formula)
+
+    """
     if not isinstance(CNF_formula, bf.And):
         raise Exception("Formula not in CNF form - CNF is not type of AND!")
-
+    """
     values = {}
 
     for clause in CNF_formula.formulas:
 
         #First check if clause is instance of OR - it has to be otherwise formula is not in CNF
+        """
         if not isinstance(clause, bf.Or):
             raise Exception("Formula is not in CNF - clause is not type of OR!")
-
+        """
         #Check if clause is empty - has no variables. It returns false and formula is not satisfiable
+        """
         if clause.isEmpty():
-            return False
-
+            raise Exception("Cannot be empty!")
+        """
         #Check if it is type of unit clause - only one literal.
-        if len(clause.formulas) == 1:
-            literal = clause.formulas[0]
+        if isinstance(clause, bf.Var):
+            values[clause.name] = bf.Tru()
+
+        elif isinstance(clause, bf.Not):
+            values[clause.formula.name] = bf.Fls()
+
+            """
             #Variable without negation - value is True
             if isinstance(literal, bf.Var):
-                values[literal.name] = True
+                values[literal.name] = bf.Tru()
             #Variable is negated - value is False
             else:
-                values[literal.formula.name] = False
+                values[literal.formula.name] = bf.Fls()
+            """
+
+    replaced_formula = CNF_formula.replace(values)
+    formula_simplified = au.simplify(replaced_formula)
+    formula_cnf = au.cnf_nnf(formula_simplified)
+
+    values_brute_force = SAT_solver_brute_force(formula_cnf, {})
 
 
-
-    return (values, CNF_formula)
+    return dict(values.items() + values_brute_force.items())
 
 
 #Returns all different variables which are found in formula
+"""
 def all_variables(CNF_formula):
-
+    print CNF_formula
     #CNF_formula has to be type of AND
     if not isinstance(CNF_formula, bf.And):
         raise Exception("Formula not in CNF form - CNF is not type of AND!")
 
-    variables = set()
+    variables = []
 
     #Iterate over each clause in CNF_formula, which has to be OR!
     for clause in CNF_formula.formulas:
@@ -65,7 +82,7 @@ def all_variables(CNF_formula):
 
             #We found variable - adding into set (no repetitions)
             if isinstance(var, bf.Var):
-                variables.add(var.name)
+                variables.append(var.name)
 
             #We found negation
             elif isinstance(var, bf.Not):
@@ -76,7 +93,7 @@ def all_variables(CNF_formula):
 
                 #Inside negation is variable - adding into set (no repetitions)
                 else:
-                    variables.add(var.formula.name)
+                    variables.append(var.formula.name)
 
             #We found OR or AND - formula is not simplified nor in CNF
             elif isinstance(var, bf.Or) or isinstance(var, bf.Not):
@@ -86,30 +103,66 @@ def all_variables(CNF_formula):
             else:
                 raise Exception("Formula is not simplified inside the clause!")
 
-    return variables
+    return au.remove_duplicates(variables)
+"""
 
+#Returns all variables which occur in formula
+def all_variables_gen(formula):
 
+    if isinstance(formula, bf.Tru):
+        return []
 
-#Solves SAT problem by trying all the possibilities for variables in formula - NOT YET WORKING
+    elif isinstance(formula, bf.Fls):
+        return []
+
+    elif isinstance(formula, bf.Not):
+        return all_variables_gen(formula.formula)
+
+    elif isinstance(formula, bf.Or):
+        variables = []
+        for clause in formula.formulas:
+            variables.extend(all_variables_gen(clause))
+        return au.remove_duplicates(variables)
+
+    elif isinstance(formula, bf.And):
+        variables = []
+        for clause in formula.formulas:
+            variables.extend(all_variables_gen(clause))
+        return au.remove_duplicates(variables)
+
+    elif isinstance(formula, bf.Var):
+        return [formula.name]
+
+#Solves SAT problem by trying all the possibilities for variables in formula - WORKING
 def SAT_solver_brute_force(CNF_formula, dictionary):
-
     #Formula is Tru / satisfiable - return dictionary with given values of variables
     if isinstance(CNF_formula, bf.Tru):
         return dictionary
 
     #Foruma is Fls -/ not satisfiable -  return False
     elif isinstance(CNF_formula, bf.Fls):
-        return False
+        return None
 
     else:
-        variables = all_variables(CNF_formula)
+        #Take first variable and set its value to True
+        variables = all_variables_gen(CNF_formula)
         dictionary[variables[0]] = bf.Tru()
-        #simplified_formula = simplify(CNF_formula, dictionary)
-        #return SAT_solver_brute_force(simplified_formula, dictionary)
+        simplified_formula = CNF_formula.replace(dictionary)
+        simplified_formula = au.simplify(simplified_formula)
+        result = SAT_solver_brute_force(simplified_formula, dictionary)
 
-        dictionary[variables[0]] = bf.Fls()
-        #simplified_formula = simplify(CNF_formula, dictionary)
-        #return SAT_solver_brute_force(simplified_formula, dictionary)
+        #Return dictionary if formula is SAT
+        if result is not None:
+            return result
+
+        #Set same variable to False and try to solve again again
+        else:
+            dictionary[variables[0]] = bf.Fls()
+            simplified_formula = CNF_formula.replace(dictionary) #simplify(CNF_formula, dictionary)
+            simplified_formula = au.simplify(simplified_formula)
+            return SAT_solver_brute_force(simplified_formula, dictionary)
+
+
 
 
 
@@ -161,10 +214,17 @@ test_CNF_formula = bf.And([bf.Or([q,p, r]), bf.Or([bf.Not(p), bf.Not(r)]), bf.Or
 
 test_CNF_formula.__repr__()
 
-dictionary = {"p": bf.Tru()}
+dictionary = {}
 
-replaced_formula = au.replace(dictionary, test_CNF_formula)
-print replaced_formula
+#replaced_formula = au.replace(dictionary, test_CNF_formula)
+#print replaced_formula
+
+#print all_variables_gen(test_CNF_formula)
+
+#solve_dict = SAT_solver_brute_force(test_CNF_formula, dictionary)
+#print solve_dict
+
+print DPLL(test_CNF_formula)
 
 """
 (values, formula) = DPLL(test_CNF_formula)
